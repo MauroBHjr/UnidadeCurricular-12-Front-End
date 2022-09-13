4d715782792be943c17544add2d4fc65809d4165
Mauro Benck Homrich Junior <182110041@senacinformatica.br>
2022-09-12 21:57:06 -0300

    trabalhando no form de cadastro e navegação do portifólio

---
+++ JavaScript/12-09-2022/script.js
@@ -0,0 +1,13 @@
+function chamaCadPF(){
+    alert("Função chama cad pf");
+    document.getElementById("CadPF").addEventListener("click", () => {
+    document.getElementById("CadForm").hidden = false;
+  }, false)
+}
+
+function chamaCadPJ(){
+    alert("Função chama cad pJ");
+}
+
+
+

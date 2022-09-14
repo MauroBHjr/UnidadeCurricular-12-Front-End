63df26fb9c1131af5ee3e46ea8e23b2f4c2dd147
Mauro Benck Homrich Junior <182110041@senacinformatica.br>
2022-09-13 21:51:27 -0300

    ainda não entedi como trabalhar com radiobutoon

--- JavaScript/12-09-2022/script.js
+++ JavaScript/12-09-2022/script.js
@@ -9,5 +9,13 @@ function chamaCadPJ(){
     alert("Função chama cad pJ");
 }
 
+function myFunction(selecionaDocumento) {
+  document.getElementById("docType").value = selecionaDocumento;
+  valueteste = document.getElementById("docType").value;
+  idteste = document.getElementById("docType").id;
+  nameteste = document.getElementById("docType").name;
+  alert("Id " + idteste + "\nValue " + valueteste + "\nName " + nameteste);
+}
+
 
 
